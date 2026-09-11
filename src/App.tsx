5f571e40d95117fd864ff79c52dbbdb1ee7c1503
TunyawatWinyu <wtunyawat@gmail.com>
2026-09-12 01:48:43 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Page/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState<boolean>(false);
+  const menu_navbar = [
+    { name: "HOME", path: "/home" },
+    { name: "MENU", path: "/menu" },
+    { name: "CHI SIAMO", path: "/chi-siamo" },
+    { name: "GALLERY", path: "/gallery" },
+    { name: "CONTATTI", path: "/contatti" },
+  ];
+
+  useEffect(() => {
+    const handleScroll = () => setScrolled(window.scrollY > 50);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <nav
+        className={`fixed top-0 left-0 z-50 w-full flex justify-between items-center transition-all duration-300 ${scrolled ? "bg-white shadow-md text-black px-[300px] py-[10px] " : "bg-transparent px-[300px] py-[20px] "}`}
+      >
+        <a className="font-heading text-2xl tracking-widest cursor-pointer">
+          OSTERIA
+          <span className="mx-3">MILANO</span>
+        </a>
+        <ul className="flex">
+          {menu_navbar.map((el) => (
+            <li className="px-4 text-md cursor-pointer" key={el.name}>
+              <NavLink
+                to={el.path}
+                className={({ isActive }) =>
+                  `relative font-semibold after:absolute after:-bottom-1 after:left-0 after:h-px after:w-0 after:bg-primary after:duration-200 after:transition-all ${isActive ? "after:w-full" : "after:w-0 hover:after:w-full"} `
+                }
+              >
+                {el.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <a
+          className={`tracking-widest ${scrolled ? "bg-black text-white px-[30px] py-[10px] font-semibold cursor-pointer transition duration-300 ease-in-out hover:bg-matcha-green hover:text-white" : "bg-white text-black px-[30px] py-[10px] font-semibold cursor-pointer transition duration-300 ease-in-out hover:bg-primary hover:text-white"}`}
+        >
+          PRENOTA UN TAVOLO
+        </a>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
